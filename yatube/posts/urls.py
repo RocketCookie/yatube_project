d8c8fecfc43c0504de06add2e8347d3325c8b882
Олег Гуров <guroff.oleg@gmail.com>
2022-11-21 22:37:30 +0300

    Added url in template

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
 
+
+app_name = 'posts'
+
 urlpatterns = [
-    path('', views.index),
-    path('index.html', views.index),
-    path('group_list.html', views.group_posts),
+    path('', views.index, name='posts_index'),
+    path('group_list.html', views.group_posts, name='group_list'),
 ]
