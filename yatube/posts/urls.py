a0ad19549327a0814038da2ae5080b6a2ecf5459
Олег Гуров <guroff.oleg@gmail.com>
2022-11-24 21:52:32 +0300

    Fixed urls and templates

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -6,6 +6,5 @@ app_name = 'posts'
 
 urlpatterns = [
     path('', views.index, name='posts_index'),
-    path('group/<slug:slug>/', views.group_posts, name='group_list'),
-    path('group_list.html/', views.group_posts, name='group_list'),
+    path('group/<slug:slug>/', views.group_posts, name='group_list'),    
 ]
